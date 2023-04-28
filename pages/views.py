601d154cf1440021f077f6c8871960e60494a6fa
1-aminadab <4ethiocihld@gmail.com>
2023-04-28 17:13:24 +0300

    crate new app 'pages'and create and route using url.py, route.py, model.py and templates

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 # Create your views here.
 
-def homePageView(request):
-    return HttpResponse('Hello, worled')
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
